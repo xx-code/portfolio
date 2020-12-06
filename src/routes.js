3b7e12ac2a4fff651ed3e6f380d2186713f8d6ab
Auguste Junior <akaaugustejunior@gmail.com>
2020-12-06 02:26:01 +0000

    init home page

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Home from './views/home';
+
 const Router = () => {
     return(
         <Switch>
-
+            <Route path="/" component={Home} />
         </Switch>
     )
 }
---
+++ src/views/home/index.js
@@ -0,0 +1,4 @@
+import Home from './home';
+import './index.scss';
+
+export default Home;
