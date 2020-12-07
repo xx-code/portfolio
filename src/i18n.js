91742e1ae7442e548571ec178aa821b4dc81261a
Auguste Junior <akaaugustejunior@gmail.com>
2020-12-07 22:44:58 +0000

    add home page

--- src/i18n.js
+++ src/i18n.js
@@ -2,10 +2,14 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import translationEN from './locales/en/translation.json';
+import translationFR from './locales/fr/translation.json';
 
 const resources = {
     en: {
         translation: translationEN
+    },
+    fr: {
+        translation: translationFR
     }
 }
 
