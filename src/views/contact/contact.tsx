69738199dfc5201de85f5724b7fabcc499bdda96
Auguste Junior <akaaugustejunior@gmail.com>
2020-12-17 16:19:23 +0000

    add contact input fields

--- src/views/contact/contact.tsx
+++ src/views/contact/contact.tsx
@@ -1,11 +1,102 @@
-import React from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import Button from '../../components/button';
+import InputText from '../../components/inputText';
 
 import './index.scss';
+import validInput from './utils/validInput';
 
 const Contact: React.FC = () => {
-    return(
-        <div>
+    const { t } = useTranslation();
+    const initInput = {
+        firstname: '',
+        lastname: '',
+        email: '',
+        describe: ''
+    }
+    const [errors, setErrors] = useState(initInput);
+    const [input, setInput] = useReducer((state: any, newState: any) => ({...state, ...newState}), initInput); 
 
+    useEffect(() => {
+        const contactComponent = document.getElementById('contact');
+        if (contactComponent) {
+            contactComponent.style.opacity = '1'; 
+        }
+    });
+
+    const handleInputFiels = (event: any) => {
+        const { name, value } = event.target;
+        setInput({[name]: value});
+    };
+
+    const onSubmit = () => {
+        const { isValid, errors } = validInput(input);
+        console.log(input)
+        console.log(errors)
+        if (isValid) {
+            setErrors(initInput);
+            console.log(input);
+        } else {
+            setErrors(errors);
+        }
+    };
+
+    return(
+        <div id="contact" className="container">
+            <div className="row">
+                <div className="content-left col-md-6">
+                    <h1>{ t(`contact_me`) }</h1>
+                    <div className="contact-form">
+                        <div className="contact-form-wrapper row">
+                            <div className="col-md-6">
+                                <InputText 
+                                    type="text"
+                                    name="firstname"
+                                    value={input.firstname}
+                                    placeholder={t('firstname')}
+                                    onChange={handleInputFiels}
+                                    error={errors.firstname} />
+                            </div>
+                            <div className="col-md-6">
+                                <InputText 
+                                    type="text"
+                                    name="lastname"
+                                    value={input.lastname}
+                                    placeholder={t('lastname')}
+                                    onChange={handleInputFiels}
+                                    error={errors.lastname} />
+                            </div>
+                            <div className="col-md-12">
+                                <InputText 
+                                    type="email"
+                                    name="email"
+                                    placeholder={t('email')}
+                                    onChange={handleInputFiels}
+                                    error={errors.email} />
+                            </div>
+                            <div className="col-md-12">
+                                <InputText 
+                                    name="describe"
+                                    value={input.describe}
+                                    placeholder={t('describe_contact')}
+                                    onChange={handleInputFiels}
+                                    error={errors.describe}
+                                    rows={6}
+                                    isTextarea />
+                            </div>
+                            <div className="col-md-12">
+                                <Button 
+                                    className="contact-button"
+                                    label={t('contact_me')}
+                                    onClick={onSubmit} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="content-right col-md-6">
+
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/views/contact/utils/validInput.ts
@@ -0,0 +1,38 @@
+import isEmpty from '../../../utils/isEmpty';
+import i18next from 'i18next';
+
+const ValidInput = (data: any) => {
+    let errors = {
+        email: '',
+        firstname: '',
+        lastname: '',
+        describe: ''
+    };
+
+    const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    if (isEmpty(data.email)) {
+        errors.email = i18next.t('email_error');
+    } else if (!regexEmail.test(data.email)) {
+        errors.email = i18next.t('email_no_valid');
+    }
+
+    if (isEmpty(data.firstname)) {
+        errors.firstname = i18next.t('firstname_error');
+    }
+
+    if (isEmpty(data.lastname)) {
+        errors.lastname = i18next.t('lastname_error');
+    }
+
+    if (isEmpty(data.describe)) {
+        errors.describe = i18next.t('describe_error');
+    }
+
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }; 
+};
+
+export default ValidInput;
