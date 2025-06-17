d9b57723670ba90bb53527d192c89b2397284808
Auguste Tiemele <32271140+xx-code@users.noreply.github.com>
2025-06-16 21:39:36 -0400

    configure nuxt with i18n

---
+++ components/Navbar/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const { locales, setLocal } = useI18n()
+</script>
+
+<template>
+    <button v-for="locale in locales" @click="setLocale(locale.code)">
+      {{ locale.name }}
+    </button>
+    <div> Navbar {{ $t('AboutMe.para_1') }}</div>
+</template>
